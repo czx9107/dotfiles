dc1c2f4e6c5a16a009cfc4c492a2036ec4e04549
czx9107 <2500017750@stu.pku.edu.cn>
2026-03-18 14:33:58 +0800

    Disabled logging in with password

--- NewServerConfig.sh
+++ NewServerConfig.sh
@@ -47,5 +47,6 @@ EOF
 
 sed -i 's/^#PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config
 sed -i 's/^#PasswordAuthentication.*/PasswordAuthentication no/' /etc/ssh/sshd_config
+systemctl restart ssh
 
 echo "Setup complete!"
